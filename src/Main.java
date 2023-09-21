ec7f39a9bacb27218ac2290217bd6d995ddd4e95
Tiago <tco.sousa@campus.fct.unl.pt>
2023-09-21 20:36:37 +0100

    Formatted main and ContactBook.

--- src/Main.java
+++ src/Main.java
@@ -6,17 +6,17 @@ import java.util.Scanner;
 
 public class Main {
     //Constantes que definem os comandos
-    public static final String ADD_CONTACT    = "AC";
+    public static final String ADD_CONTACT = "AC";
     public static final String REMOVE_CONTACT = "RC";
-    public static final String GET_PHONE      = "GP";
-    public static final String GET_EMAIL      = "GE";
-    public static final String SET_PHONE      = "SP";
-    public static final String SET_EMAIL      = "SE";
-    public static final String LIST_CONTACTS  = "LC";
+    public static final String GET_PHONE = "GP";
+    public static final String GET_EMAIL = "GE";
+    public static final String SET_PHONE = "SP";
+    public static final String SET_EMAIL = "SE";
+    public static final String LIST_CONTACTS = "LC";
 
     public static final String LOOKUP_CONTACT = "GN";
     public static final String PHONE_EQUALS_VERIFY = "EQ";
-    public static final String QUIT           = "Q";
+    public static final String QUIT = "Q";
 
     //Constantes que definem as mensagens para o utilizador
     public static final String CONTACT_EXISTS = "contactBook.Contact already exists.";
@@ -33,25 +33,25 @@ public class Main {
         ContactBook cBook = new ContactBook();
         String comm = getCommand(in);
 
-        while (!comm.equals(QUIT)){
+        while (!comm.equals(QUIT)) {
             switch (comm) {
                 case ADD_CONTACT:
-                    addContact(in,cBook);
+                    addContact(in, cBook);
                     break;
                 case REMOVE_CONTACT:
-                    deleteContact(in,cBook);
+                    deleteContact(in, cBook);
                     break;
                 case GET_PHONE:
-                    getPhone(in,cBook);
+                    getPhone(in, cBook);
                     break;
                 case GET_EMAIL:
-                    getEmail(in,cBook);
+                    getEmail(in, cBook);
                     break;
                 case SET_PHONE:
-                    setPhone(in,cBook);
+                    setPhone(in, cBook);
                     break;
                 case SET_EMAIL:
-                    setEmail(in,cBook);
+                    setEmail(in, cBook);
                     break;
                 case LIST_CONTACTS:
                     listAllContacts(cBook);
@@ -79,13 +79,13 @@ public class Main {
         int phone;
 
         name = in.nextLine();
-        phone = in.nextInt(); in.nextLine();
+        phone = in.nextInt();
+        in.nextLine();
         email = in.nextLine();
         if (!cBook.hasContact(name)) {
             cBook.addContact(name, phone, email);
             System.out.println(CONTACT_ADDED);
-        }
-        else System.out.println(CONTACT_EXISTS);
+        } else System.out.println(CONTACT_EXISTS);
     }
 
     private static void deleteContact(Scanner in, ContactBook cBook) {
@@ -94,8 +94,7 @@ public class Main {
         if (cBook.hasContact(name)) {
             cBook.deleteContact(name);
             System.out.println(CONTACT_REMOVED);
-        }
-        else System.out.println(NAME_NOT_EXIST);
+        } else System.out.println(NAME_NOT_EXIST);
     }
 
     private static void getPhone(Scanner in, ContactBook cBook) {
@@ -103,8 +102,7 @@ public class Main {
         name = in.nextLine();
         if (cBook.hasContact(name)) {
             System.out.println(cBook.getPhone(name));
-        }
-        else System.out.println(NAME_NOT_EXIST);
+        } else System.out.println(NAME_NOT_EXIST);
     }
 
     private static void getEmail(Scanner in, ContactBook cBook) {
@@ -112,20 +110,19 @@ public class Main {
         name = in.nextLine();
         if (cBook.hasContact(name)) {
             System.out.println(cBook.getEmail(name));
-        }
-        else System.out.println(NAME_NOT_EXIST);
+        } else System.out.println(NAME_NOT_EXIST);
     }
 
     private static void setPhone(Scanner in, ContactBook cBook) {
         String name;
         int phone;
         name = in.nextLine();
-        phone = in.nextInt(); in.nextLine();
+        phone = in.nextInt();
+        in.nextLine();
         if (cBook.hasContact(name)) {
-            cBook.setPhone(name,phone);
+            cBook.setPhone(name, phone);
             System.out.println(CONTACT_UPDATED);
-        }
-        else System.out.println(NAME_NOT_EXIST);
+        } else System.out.println(NAME_NOT_EXIST);
     }
 
     private static void setEmail(Scanner in, ContactBook cBook) {
@@ -134,42 +131,37 @@ public class Main {
         name = in.nextLine();
         email = in.nextLine();
         if (cBook.hasContact(name)) {
-            cBook.setEmail(name,email);
+            cBook.setEmail(name, email);
             System.out.println(CONTACT_UPDATED);
-        }
-        else System.out.println(NAME_NOT_EXIST);
+        } else System.out.println(NAME_NOT_EXIST);
     }
 
     private static void listAllContacts(ContactBook cBook) {
         if (cBook.getNumberOfContacts() != 0) {
             cBook.initializeIterator();
-            while( cBook.hasNext() ) {
+            while (cBook.hasNext()) {
                 Contact c = cBook.next();
                 System.out.println(c.getName() + "; " + c.getEmail() + "; " + c.getPhone());
             }
-        }
-        else System.out.println(BOOK_EMPTY);
+        } else System.out.println(BOOK_EMPTY);
     }
-    private static void findContactByNumber(Scanner in, ContactBook cBook){
-        //todo: check if when has the same number return the last occurrence
+
+    private static void findContactByNumber(Scanner in, ContactBook cBook) {
         //TODO: test with junit
         int phone = in.nextInt();
         in.nextLine();
         Contact contact = cBook.findByNumber(phone);
-    if( contact == null)
-        System.out.println("Phone number does not exist");
-    else{
-        System.out.println(contact.getPhone());
-        System.out.println(contact.getName());
-
-        }
+        if (contact == null)
+            System.out.println("Phone number does not exist");
+        else
+            System.out.println(contact.getName());
     }
 
     private static void checkRepeats(ContactBook cBook) {
         cBook.initializeIterator();
-        while(cBook.hasNext()) {
+        while (cBook.hasNext()) {
             int phoneNumber = cBook.next().getPhone();
-            if(cBook.countNumber(phoneNumber) != 1) {
+            if (cBook.countNumber(phoneNumber) != 1) {
                 System.out.println("There are contacts that share phone numbers.");
                 break;
             }
@@ -177,8 +169,4 @@ public class Main {
         System.out.println("All contacts have different phone numbers");
     }
 }
-
-
-
-
 
